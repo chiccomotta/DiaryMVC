42aceafcf6500347d7fbec33626bab991de9646c
Cristiano <cristiano.motta@hotmail.it>
2025-01-16 10:50:11 +0100

    Implemented CRUD operations

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,23 +1,15 @@
-﻿using System.Diagnostics;
-using Diary.Domain.Models;
-using DiaryApp.Models;
+﻿using Diary.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace DiaryApp.Controllers;
 
-public class DiaryEntriesController : Controller
+public class DiaryEntriesController(DiaryDbContext dbContext) : Controller
 {
-    private readonly DiaryDbContext _dbContext;
-    
-    public DiaryEntriesController(DiaryDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-    
     public async Task<IActionResult> Index()
     {
-        var entries = await _dbContext.DiaryEntries.ToListAsync();
+        var entries = await dbContext.DiaryEntries.ToListAsync();
         return View(entries);
     }
 
@@ -26,10 +18,76 @@ public class DiaryEntriesController : Controller
         return View();
     }
 
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        var entry = dbContext.DiaryEntries.Find(id);
+        
+        if (entry is null)
+        {
+            return NotFound();
+        }
+        
+        return View(entry);
+    }
+
+
     [HttpPost]
-    public IActionResult Create(DiaryEntry request)
+    public async Task<IActionResult> Edit(int? id, DiaryEntry request)
+    {
+        var entity = await dbContext.DiaryEntries.FindAsync(id);
+
+        if (entity is null)
+        {
+            return NotFound();
+        }
+
+        // update entity
+        
+        entity.Title = request.Title;
+        entity.Content = request.Content;
+        entity.CreateDate = request.CreateDate;
+
+        dbContext.DiaryEntries.Update(entity);
+        await dbContext.SaveChangesAsync();
+        
+        return View(entity);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        DiaryEntry? diaryEntry = dbContext.DiaryEntries.Find(id);
+
+        if (diaryEntry == null)
+        {
+            return NotFound();
+        }
+
+        return View(diaryEntry);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(DiaryEntry obj)
+    {
+
+        dbContext.DiaryEntries.Remove(obj);     // Remove the diary entry from the database
+        dbContext.SaveChanges();                // Saves the changes to the database
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(DiaryEntry request)
     {
         Debug.WriteLine(request);
-        return View();
+        dbContext.DiaryEntries.Add(request);
+
+        await dbContext.SaveChangesAsync();
+        return RedirectToAction("Index");
     }
 }
